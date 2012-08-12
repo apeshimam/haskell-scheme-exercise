1ac79e548168ca9d652d3f83cdde360504fa79ea
Artie Pesh-Imam <apeshimam@gmail.com>
2012-08-12 03:18:42 -0700

    added additional numeric format support

--- simple_parser.hs
+++ simple_parser.hs
@@ -16,18 +16,29 @@ main = do args <- getArgs
           putStrLn (readExpr (args !! 0))
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 escapedChars :: Parser Char
 escapedChars = do char '\\'
-                  x <- oneOf "\\\""
-                  return x
+                  x <- oneOf "\\\"nrt"
+                  return $ case x of 
+                    '\\' -> x
+                    '"'  -> x
+                    'n'  -> '\n'
+                    'r'  -> '\t' 
 
 parseString :: Parser LispVal
 parseString = do char '"'
                  x <- many $ escapedChars <|> noneOf "\"\\"
                  char '"'
                  return $ String x
+
+parseBool :: Parser LispVal 
+parseBool = oneOf string "#"
+                  x <- oneOf "tf"
+                  return $ case x of 
+                    't' -> Bool True
+                    'f' -> Fool False
 
 parseAtom :: Parser LispVal
 parseAtom = do first <- letter <|> symbol
@@ -38,13 +49,47 @@ parseAtom = do first <- letter <|> symbol
                           "#f" -> Bool False
                           _    -> Atom atom
 
+parseDigital1 :: Parser LispVal
+parseDigital1 = do x many1 digit
+                   (return . Number . read) x
+
+parseDigital2 :: Parser LispVal
+parseDigital2 = do try $ string "#d"
+                   x <- many1 digit
+                   (return . Number . read) x
+
+parseHex :: Parser LispVal
+parseHex = do try $ string "#x"
+            x <- many1 hexDigit
+            return $ Number (hex2dig x)
+
+parseOct :: Parser LispVal
+parseOct = do try $ string "#o"
+              x <- many1 octDigit
+              return $ Number (oct2dig x)
+
+parseBin :: Parser LispVal
+parseBin = do try $ string "#b"
+              x <- many1 (oneOf "10")
+              return $ Number (bin2dig x)
+
+oct2dig x = fst $ readOct x !! 0
+hex2dig x = fst $ readHex x !! 0
+bin2dig  = bin2dig' 0
+bin2dig' digint "" = digint
+bin2dig' digint (x:xs) = let old = 2 * digint + (if x == '0' then 0 else 1) in
+                         bin2dig' old xs
+                         
+
 parseNumber :: Parser LispVal
-parseNumber = many1 digit >>= \num -> (return . Number . read) num
+parseNumber = do num <- parseDigital1 <|> parseDigital2 <|> parseHex <|> parseOct <|> parseBin
+                 return $ num
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
          <|> parseString
          <|> parseNumber
+         <|> parseBool
 
 
 spaces :: Parser ()
